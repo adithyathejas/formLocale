2776b210be9ce1b6711bee791a572a5e16514964
adithyathejas <adithyathejus00@gmail.com>
2022-08-21 17:58:39 +0530

    object2

--- main.js
+++ main.js
@@ -3,16 +3,36 @@ var username = document.getElementById("name")
 const email = document.getElementById("email")
 
 form.addEventListener("submit",onSubmit);
-myObj = {
-    name : username.value,
-    email: email.value
-}
+window.addEventListener("beforeunload", handleBeforeUnload);
+
+
+
+
 
 function onSubmit(e)
 {
+    e.preventDefault()
    
-    
-    localStorage.setItem("user",myObj)
+    myObj = {
+        name : username.value,
+        email: email.value
+    }
 
+    var myObj_serial = JSON.stringify(myObj);
+
+    localStorage.setItem("user",myObj_serial);
+
+    var myObj_deserial = JSON.parse(myObj_serial); 
+    alert(myObj_deserial.name,myObj_deserial.email)
 }
+
+
+function handleBeforeUnload(e){
+    e.preventDefault();
+    const message =
+    "Are you sure you want to leave? All provided data will be lost.";
+  e.returnValue = message;
+  return message;
+}
+
 
