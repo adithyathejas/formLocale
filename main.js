64de0a6abe0138233311784b77aaa99579aa9d98
adithyathejas <adithyathejus00@gmail.com>
2022-09-03 17:11:47 +0530

    dfssdfsdf

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-// let multiply = function(x,y){
-//     console.log(x*y);
-// }
+class student {
+    static count = 0
+    constructor(name,age,phone,marks){
+        this.name =  name
+        this.age= age
+        this.phone= phone
+        this.marks = marks
+    }
+   
+}
 
-// let multiplyByTwo = multiply.bind(this,2)
-// let multiplyByThree = multiply.bind(this,3)
-// multiplyByTwo(5)
-// multiplyByThree(5)
+student.prototype.isEligible = function(minmark){
+    return (minage)=>{
+        if (this.age > minage && this.marks>minmark){
+            console.log("placed")
+        }
+        else{
+            console.log("not placed")
+        }
 
-let multiply =function(x){
-    return function(y){
-       console.log(x*y)
     }
 }
 
-let  multiplyByTwo = multiply(2)
-multiplyByTwo(5)
+let ajith = new student('ajith',28,8078371316,50)
+let anooja = new student('anooja',20,8078371316,45)
+let akhil = new student('akhil',24,701525465,30)
+let aji = new student('aji',21,7837131680,20)
+let aju = new student('aju',28,8078371316,30)
 
-let multiplyByThree = multiply(3)
-multiplyByThree(5)
-
+ajith.isEligible(20)(18)
+anooja.isEligible(14)(47)
